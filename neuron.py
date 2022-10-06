a62b69584addaedb12edca338a4b7bf2efb38cb1
christosk99 <christoskyriazis@hotmail.co.uk>
2022-10-06 11:20:50 +0200

    livegraphing works, kinda wanna give up on this idea as too many unoptimal decisions for my ideas.

--- neuron.py
+++ neuron.py
@@ -6,9 +6,9 @@ import time
 import multiprocessing
 
 
-#TODO:  model refractory period with function (1ms/0.001s), explore live graphing in matplotlib, make neurons spike again, allow for multineuron connections.
+#TODO:  model refractory period with function (1ms/0.001s), make neurons spike correctly, allow for multineuron connections.
 
-#slowed down x100 since we use 0.1s intervals ?? unsure
+#program depends on processing speed to be fast enough for stupid line80 to work. Adjust 0.1[better]-0.15[worse] accordingly.
 
 class Neuron():
     def __init__(self, name, fig, ax):
@@ -76,10 +76,10 @@ class Neuron():
 
 
             print(self.V_hist[-1])
-            #wait for time to reach +100ms from curr_time
-            amt = 0.1 - (time.time()-curr_time)
+            #really inaccurate way to make this work
+            amt = 0.15 - (time.time()-curr_time)
             #throttle the program based on computing time
-            #time.sleep(amt)
+            time.sleep(amt)
 
             if self.spike:
                 for i in range(int(1./0.1)): #better solution needed although replaced once func for refrac. per.
@@ -89,20 +89,18 @@ class Neuron():
                     self.graph.set_ydata(self.graph_data)
                     self.fig.canvas.draw()
                     self.fig.canvas.flush_events()
-                    #time.sleep(0.1) #refractory period slowed down x1000
+                    time.sleep(0.1)
                 V = self.calc_V()
                 self.V_hist.append(V)
                 self.spike = False
         
-        #plt.plot(self.V_hist)
-        #plt.show()
         del self
 
 def main():
     plt.ion()
     fig, ax = plt.subplots(figsize=(12,6), facecolor='#DEDEDE')
-    plt.title("TEStsadgagd")
-    plt.xlabel("time window")
+    plt.title("livegraph neuron")
+    plt.xlabel("time window (0.2s)")
     plt.ylabel("V")
     ax.set_ylim(-85.,-55.)
     neu1 = Neuron('neu1', fig, ax)
